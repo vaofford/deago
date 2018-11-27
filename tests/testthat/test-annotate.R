0e76bf73bc5d378e2878555f973894b107b13bce
vaofford <vo1@sanger.ac.uk>
2018-11-27 11:57:05 +0000

    update annotation tests

--- tests/testthat/test-annotate.R
+++ tests/testthat/test-annotate.R
@@ -1,15 +1,20 @@
 context("annotation")
 
 test_that("importing annotation and annotating DESeqDataSet works", {
-  expect_error(annotateDataset())
+  expect_error(annotateDataset(), 'argument "dds" is missing, with no default')
+  expect_error(annotateDataset(expected_dds_de), 'argument "parameters" is missing, with no default')
+  expect_error(annotateDataset(expected_dds_de, list()), "Could not import annotation: no annotation file provided.")
+  expect_error(annotateDataset(expected_dds_de, bad_annotation_parameters), "Could not import annotation: no dataset identifiers found.")
   expect_silent(observed_dds_de_ann <- annotateDataset(expected_dds_de, expected_parameters))
   expect_identical(observed_dds_de_ann, expected_dds_de_ann)
 })
 
 test_that("importing annotation works", {
-  expect_error(importAnnotation())
+  expect_error(importAnnotation(), 'argument "file" is missing, with no default')
+  expect_error(importAnnotation('noFile'), 'noFile does not exist')
   expect_silent(observed_annotation <- importAnnotation(expected_parameters$annotation_file))
   expect_identical(observed_annotation, expected_annotation)
+  expect_silent(observed_annotation <- importAnnotation(switched_annotation_file))
 })
 
 test_that("importing annotation with too few columns fails", {
@@ -34,6 +39,14 @@ test_that("adding annotation with GO terms in multiple columns fails", {
 test_that("adding annotation with 3 columns and no GO terms fails", {
   bad_annotation <- cbind(expected_annotation[,1:2], c(1:1000))
   expect_error(addAnnotationsToDataSet(expected_dds_de, bad_annotation), "Could not import annotation: no GO terms identified.")
+})
+
+test_that("adding annotation with wrong column number fails", {
+  bad_annotation <- data.frame('x'=1:5)
+  expect_error(addAnnotationsToDataSet(expected_dds_de, bad_annotation), "Could not import annotation: too few columns in annotation.")
+  
+  bad_annotation <- data.frame('a'=1:5,'b'=1:5,'c'=1:5,'d'=1:5)
+  expect_error(addAnnotationsToDataSet(expected_dds_de, bad_annotation), "Could not import annotation: too many columns in annotation.")
 })
 
 test_that("adding annotation with genes only works", {
